faae021f1a0c21834179cd42d8b92c66410c0135
Gustavo Kapibara <gustavo@kapibara.com.br>
2021-10-14 11:44:07 -0300

    add avatar

--- projects/avatar/src/lib/avatar.component.spec.ts
+++ projects/avatar/src/lib/avatar.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AvatarComponent } from './avatar.component';
 
@@ -6,12 +6,12 @@ describe('AvatarComponent', () => {
   let component: AvatarComponent;
   let fixture: ComponentFixture<AvatarComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [ AvatarComponent ]
     })
     .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AvatarComponent);
--- projects/avatar/src/lib/avatar.service.spec.ts
+++ projects/avatar/src/lib/avatar.service.spec.ts
@@ -3,10 +3,14 @@ import { TestBed } from '@angular/core/testing';
 import { AvatarService } from './avatar.service';
 
 describe('AvatarService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AvatarService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AvatarService);
+  });
 
   it('should be created', () => {
-    const service: AvatarService = TestBed.get(AvatarService);
     expect(service).toBeTruthy();
   });
 });
