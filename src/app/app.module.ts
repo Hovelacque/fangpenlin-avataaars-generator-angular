f07ac4532ab63e5ee61b02ab26aa7c5be7c4f862
Gustavo Kapibara <gustavo@kapibara.com.br>
2021-10-14 11:43:57 -0300

    new project angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatRadioModule} from '@angular/material/radio';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {MatSelectModule} from '@angular/material/select';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {CommonModule} from '@angular/common';
-import {MatDividerModule} from '@angular/material/divider';
-import {AngularFontAwesomeModule} from 'angular-font-awesome';
-import { RouterModule } from '@angular/router';
-import {MatIconModule} from '@angular/material/icon';
-import {AvatarComponent} from '../../projects/avatar/src/public-api';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AvatarComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatRadioModule,
-    FormsModule,
-    MatSelectModule,
-    MatFormFieldModule,
-    MatIconModule,
-    CommonModule,
-    ReactiveFormsModule,
-    MatDividerModule,
-    MatButtonModule,
-    AngularFontAwesomeModule,
-    RouterModule.forRoot(
-      [
-        { path: "", component: AppComponent}
-      ]
-    )
+    BrowserModule
   ],
-  providers: [
-    
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
